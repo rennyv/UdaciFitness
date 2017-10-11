e2a5ee327607fb85b3b3a76f84cb7b02e4aa19b4
Renny Vanneste <renny@vanneste.ca>
2017-10-10 21:45:59 -0600

    added a reset button outline

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
+import { Ionicons } from '@expo/vector-icons'
 
 
 function SubmitBtn ({ onPress }) {
@@ -74,8 +76,35 @@ export default class AddEntry extends Component {
     // Clear local notification 
   }
   
+  reset = () => {
+    const key = timeToString()
+
+    //Update Redux
+
+    //Route to home
+
+    //Update DB
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name='ios-happy-outline'
+            size={100}
+          />
+          <Text>
+            You alreafy logged your information for today
+          </Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
