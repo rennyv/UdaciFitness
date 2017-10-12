acf7ba49322556c8e7b33505824cd084f9d2f604
Renny <renny@bunnyhug.net>
2017-10-11 21:17:19 -0600

    added in AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,7 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import TextButton from './TextButton'
 import { Ionicons } from '@expo/vector-icons'
-
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -71,7 +71,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to 'DB'
+    submitEntry({key, entry})
 
     // Clear local notification 
   }
@@ -83,7 +83,7 @@ export default class AddEntry extends Component {
 
     //Route to home
 
-    //Update DB
+    removeEntry(key)
   }
 
   render() {
