3fd7085227ce258b82829d9d38ee26e9071236e7
Renny Vanneste <renny@vanneste.ca>
2017-10-17 22:06:24 -0600

    added denied screen

--- components/Live.js
+++ components/Live.js
@@ -7,12 +7,12 @@ import { purple, white } from '../utils/colors'
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'undetermined',
+    status: 'denied',
     direction: ''
   }
 
   askPermission = () => {
-    
+
   }
 
   render() {
@@ -24,8 +24,11 @@ export default class Live extends Component {
 
     if (status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} /> 
+          <Text>
+            You denied your location. You can fix this by visitting your settings and enabling lcoation services for this app.
+          </Text>
         </View>
       )
     }
